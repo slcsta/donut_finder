5cbd88c8f3049f81bb425d72bd78e942f7093d99
sarahchavez <sarahchavez@cvent.com>
2022-11-08 04:32:43 -0800

    merged remote main to update branch IAM-673

--- packages/app/CHANGELOG.md
+++ packages/app/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @slcsta/donut-finder
 
+## 0.2.0
+
+### Minor Changes
+
+- 3f0644a: Creating Flask app with navigation to the landing page and Jinja templating.
+
 ## 0.1.0
 
 ### Minor Changes
---
+++ packages/app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        city = request.form.get("city")
+        state = state.form.get("state")
+        return redirect("/")
+
+    else:
+        return render_template("index.html")

6c2f5823c131c91568ba9e17a6c18348b656b2f2
sarahchavez <sarahchavez@cvent.com>
2022-11-09 11:26:13 -0800

    seeded db

--- packages/app/init_db.py
+++ packages/app/init_db.py
@@ -1,10 +1,23 @@
 import sqlite3 as sql
+import json
+
+with open('response.json') as f:
+    data = json.load(f)
 
 connection = sql.connect("dounut_shops.db")
 
-with open('schema.sql') as f:
-    connection.executescript(f.read())
+with open('schema.sql') as s:
+    connection.executescript(s.read())
 
 cursor = connection.cursor()
+
+with open('response.json') as f:
+    data = json.load(f)
+
+businesses = data["businesses"]
+
+for business in businesses:
+    cursor.execute("INSERT INTO shops (name, website, rating, phone) VALUES (?, ?, ?, ?)",
+    (business["name"], business["url"], business["rating"], business["display_phone"]))
 
 connection.close()

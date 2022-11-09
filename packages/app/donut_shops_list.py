b82504e89984bc6b972a028f832181a22abb9e26
sarahchavez <sarahchavez@cvent.com>
2022-11-09 06:43:23 -0800

    updated donut_shops_list to include all table values

--- packages/app/donut_shops_list.py
+++ packages/app/donut_shops_list.py
@@ -7,8 +7,16 @@ data = json.load(f)
 businesses = data["businesses"]
 
 for business in businesses:
-    print("name:", business["name"])
-    print("website:", business["url"])
-    print("\n")
+    name = business["name"]
+    website = business["url"]
+    rating = business["rating"]
+    city = " ".join(business["location"]["city"])
+    state = " ".join(business["location"]["city"])
+    address = " ".join(business["location"]["display_address"])
+    phone = business["display_phone"]
+    print(name, website, rating)
+
+    f.close()
+    
     
 

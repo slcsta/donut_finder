f10c31ac2c51c4f573008989d0aac9faa49a1970
sarahchavez <sarahchavez@cvent.com>
2022-11-09 04:17:25 -0800

    deleted db connection code from app.py

--- packages/app/app.py
+++ packages/app/app.py
@@ -3,14 +3,6 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-# connection = sql.connect("dounut_shops.db")
-
-# cursor = connection.cursor()
-
-# cursor.execute('CREATE TABLE donut_shops (id INTEGER PRIMARY KEY, name TEXT, address1 TEXT, city TEXT, zip_code TEXT, state TEXT, display_address TEXT, display_phone TEXT, url TEXT)')
-
-# connection.close()
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":

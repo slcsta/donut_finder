26af6d54df048f9e1a3566f7566b810e88bb9576
sarahchavez <sarahchavez@cvent.com>
2022-11-09 08:55:09 -0800

    added db connection to flask app

--- packages/app/app.py
+++ packages/app/app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sql.connect('donut_shops.db')
+    conn.row_factory = sql.row_factory
+    return conn
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":

24ef9f423f0b28c6cd1dee9b0cf4adf56a2a1a24
sarahchavez <sarahchavez@cvent.com>
2022-11-10 10:12:50 -0800

    moved post method from index to shop func, added notes and todo

--- packages/app/app.py
+++ packages/app/app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect
 # Configure application
 app = Flask(__name__)
 
-# Create a connect to db function here
+# TODO Create a connect to db function here
 
 
 # Displays all db entries on index.html
@@ -17,14 +17,14 @@ def index():
     shops = cursor.execute("SELECT * FROM shops")
     return render_template("index.html", shops=shops)
 
-
-
+@app.route("/shop", methods=["GET", "POST"])
+def shop():
     # When form is submitted via Post
-    # TODO: Look up donut shops by calling a lookup func
-    # TODO: Require that a user enter a city and select state from dropdown
-    #if request.method == "POST":
+    # TODO Call a lookup func that searches donut shops by city & state 
+    # TODO Require that a user enter a city and select state from dropdown
+    if request.method == "POST":
     #   city = request.form.get("city")
-    #    state = request.form.get("state")
-    #    return redirect("/")
+    #   state = request.form.get("state")
+    
 
         

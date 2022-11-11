36e961f8b406addce9da8b8d7a04d7d812afccb8
sarahchavez <sarahchavez@cvent.com>
2022-11-11 05:31:11 -0800

    added pseudo code on how to complete search function

--- packages/app/app.py
+++ packages/app/app.py
@@ -17,11 +17,19 @@ def index():
     shops = cursor.execute("SELECT * FROM shops")
     return render_template("index.html", shops=shops)
 
+# Displays all db entries on searched.html matching city & state inputs
+# Submit user's input via POST to /search
 @app.route("/search", methods=["GET", "POST"])
 def search():
-    # Submit user's input via POST to /search
-    # TODO Call a lookup func that searches donut shops by city & state 
-    # TODO Require that a user enter a city and select state from dropdown
+    # TODO Call a lookup func that searches donut shops by city & state
+        # Search value should equal form's inputted city
+        # Second search value should contian a state selection from the dropdown menu
+        # If city textbox input value is empty or if city matches none of the citys in db,
+        # Then return an error stating "please enter a valid city"
+        # Else return rendered searched template with lookup city & state values passed in
+    
+
+
     if request.method == "POST":
         city = request.form.get("city")
         state = request.form.get("state")

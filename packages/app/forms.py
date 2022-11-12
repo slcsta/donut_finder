7c91d8cd21ad82d7789333de15fb45521bf00efb
sarahchavez <sarahchavez@cvent.com>
2022-11-12 05:20:18 -0800

    added SelectField to wtforms import

--- packages/app/forms.py
+++ packages/app/forms.py
@@ -1,3 +1,3 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField
+from wtforms import StringField, TextField, SelectField, SubmitField
 from wtforms.validators import DataRequired

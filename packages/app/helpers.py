9d78af7ea02ad1e42d2d7ac69bb61e6eedc2fb8a
sarahchavez <sarahchavez@cvent.com>
2022-11-15 05:53:13 -0800

    removed p tag from form and added escape characters in apology helper

--- packages/app/helpers.py
+++ packages/app/helpers.py
@@ -1,4 +1,10 @@
 from flask import redirect, render_template
+from functools import wraps
 
 def apology(message, code=400):
-    return render_template("apology.html", top=code), code
+    def escape(s):
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code

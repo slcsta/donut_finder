7990094ca8dfcc8f8316f7266a43d2b5f28be923
sarahchavez <sarahchavez@cvent.com>
2022-11-16 06:37:06 -0800

    deleted escape character function

--- packages/app/helpers.py
+++ packages/app/helpers.py
@@ -1,10 +1,7 @@
 from flask import redirect, render_template
 from functools import wraps
 
+# From CS50 Example
+
 def apology(message, code=400):
-    # def escape(s):
-    #     for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-    #                      ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-    #         s = s.replace(old, new)
-    #     return s
     return render_template("apology.html", message=message, code=code), code

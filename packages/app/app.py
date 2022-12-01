6ffbc7c7d94bf59bb1b3d536891c1ca785028011
sarahchavez <sarahchavez@cvent.com>
2022-12-01 06:49:28 -0800

    added todo over states to pass each state into a job

--- packages/app/app.py
+++ packages/app/app.py
@@ -78,10 +78,10 @@ def db_connect():
     connection.row_factory = sql.Row
     return connection
 
-# Displays all db entries on index.html
-# Conditionally renders donut shops by city & state on search.html
+# Displays all db entries on index.html & renders donut shops by city & state on search.html
 @app.route("/", methods=['GET'])
 def index():
+    # TODO Pass states in dynamically for each job in apscheduler - need to increase scope (move to top)
     states = [('AL', 'Alabama'), ('AK', 'Alaska'), ('AZ', 'Arizona'), ('AR', 'Arkansas'), ('CA', 'California'), ('CO', 'Colorado'), ('CT', 'Connecticut'),
         ('DE', 'Delaware'), ('FL', 'Florida'), ('GA', 'Georgia'), ('HI', 'Hawaii'), ('ID', 'Idaho'), ('IL', 'Illinois'), ('IN', 'Indiana'), ('IA', 'Iowa'),
         ('KS', 'Kansas'), ('KY', 'Kentucky'), ('LA', 'Louisiana'), ('ME', 'Maine'), ('MD', 'Maryland'), ('MA', 'Massachusetts'), ('MI', 'Michigan'), 

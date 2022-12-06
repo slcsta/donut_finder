893a9550ec676319d7023ce17d284517fe7d4d7e
sarahchavez <sarahchavez@cvent.com>
2022-12-06 10:02:42 -0800

    altered shops table to add unique constraint on address

--- packages/app/sql/001.sql
+++ packages/app/sql/001.sql
@@ -5,30 +5,30 @@
 --     GROUP BY address
 -- );
 
-SELECT * FROM shops ORDER BY state, city, name, address, rating;
+--SELECT * FROM shops ORDER BY state, city, name, address, rating;
 --SELECT * FROM shops ON CONFLICT(address) DO UPDATE SET ignores ORDER BY state, city, name, address, rating;
---PRAGMA foreign_keys=off;
+PRAGMA foreign_keys=off;
 
--- BEGIN TRANSACTION;
+BEGIN TRANSACTION;
 
--- ALTER TABLE shops RENAME TO old_shops;
+ALTER TABLE shops RENAME TO old_shops;
 
--- CREATE TABLE shops (   
---     id INTEGER PRIMARY KEY AUTOINCREMENT,
---     name TEXT,
---     website TEXT,
---     rating REAL,
---     address TEXT,
---     address2 TEXT, 
---     city TEXT,  
---     state TEXT, 
---     zip_code TEXT,
---     phone TEXT, 
---     CONSTRAINT address_unique UNIQUE (address)
--- );
+CREATE TABLE shops (   
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    website TEXT,
+    rating REAL,
+    address TEXT,
+    address2 TEXT, 
+    city TEXT,  
+    state TEXT, 
+    zip_code TEXT,
+    phone TEXT, 
+    CONSTRAINT address_unique UNIQUE (address)
+);
 
--- INSERT INTO shops SELECT * FROM old_shops;
+INSERT INTO shops SELECT * FROM old_shops;
 
--- COMMIT;
+COMMIT;
 
---PRAGMA foreign_keys=on;
+PRAGMA foreign_keys=on;
